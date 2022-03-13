48e3233a8d49b1b8e2cf9e11717f8036da427a46
Jolanta Peisokaitė <jolanta.peisokaite@gmail.com>
2022-03-13 22:33:06 +0200

    Completed 3 part

---
+++ src/part3/MainPart3.java
@@ -0,0 +1,14 @@
+package part3;
+
+import part1.uni_dev.Point;
+
+public class MainPart3 {
+    public static void main(String[] args) {
+        Point point1 = new Point(2,4);
+        Point point2 = new Point(4,4);
+
+        Line line = new Line(point1, point2);
+        line.toStringLine();
+        System.out.println(line.distance());
+    }
+}
